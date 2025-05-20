5d02917bd1a23e5a59ac3045ff8da417bcaec956
honda-yuki-9134 <585-student019@la-study.com>
2025-05-20 09:47:30 +0900

    form

--- seven/delivery/GoodsList.Designer.cs
+++ seven/delivery/GoodsList.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace seven
+﻿namespace seven.delivery
 {
-    partial class CustomerList
+    partial class GoodsList
     {
         /// <summary>
         /// Required designer variable.
@@ -29,20 +29,19 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button1 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -51,130 +50,115 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
-            this.Column5,
             this.Column2,
             this.Column3,
             this.Column4});
-            this.dataGridView1.Location = new System.Drawing.Point(20, 110);
+            this.dataGridView1.Location = new System.Drawing.Point(29, 107);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowTemplate.Height = 21;
-            this.dataGridView1.Size = new System.Drawing.Size(507, 214);
+            this.dataGridView1.Size = new System.Drawing.Size(404, 209);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(27, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(124, 27);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "顧客一覧";
             // 
             // Column1
             // 
-            this.Column1.HeaderText = "顧客No";
+            this.Column1.HeaderText = "商品ID";
             this.Column1.Name = "Column1";
-            // 
-            // Column5
-            // 
-            this.Column5.HeaderText = "メールアドレス";
-            this.Column5.Name = "Column5";
             // 
             // Column2
             // 
-            this.Column2.HeaderText = "名前";
+            this.Column2.HeaderText = "商品名";
             this.Column2.Name = "Column2";
             // 
             // Column3
             // 
-            this.Column3.HeaderText = "住所";
+            this.Column3.HeaderText = "価格";
             this.Column3.Name = "Column3";
             // 
             // Column4
             // 
-            this.Column4.HeaderText = "電話番号";
+            this.Column4.HeaderText = "カテゴリーNo";
             this.Column4.Name = "Column4";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(318, 83);
+            this.button1.Location = new System.Drawing.Point(293, 58);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 21);
-            this.button1.TabIndex = 2;
+            this.button1.Size = new System.Drawing.Size(126, 36);
+            this.button1.TabIndex = 1;
             this.button1.Text = "検索";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(47, 83);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 12);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "名前：";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(88, 80);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(200, 19);
-            this.textBox1.TabIndex = 4;
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(3, 372);
+            this.button2.Location = new System.Drawing.Point(12, 351);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(107, 33);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "編集";
+            this.button2.Size = new System.Drawing.Size(126, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "新規追加";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(133, 372);
+            this.button3.Location = new System.Drawing.Point(167, 351);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(107, 33);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "新規登録";
+            this.button3.Size = new System.Drawing.Size(126, 36);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "編集";
             this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(18, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 27);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "商品管理";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "カテゴリーNo：";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(129, 68);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 19);
+            this.numericUpDown1.TabIndex = 6;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(265, 372);
+            this.button4.Location = new System.Drawing.Point(338, 351);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(107, 33);
+            this.button4.Size = new System.Drawing.Size(122, 36);
             this.button4.TabIndex = 7;
-            this.button4.Text = "削除";
+            this.button4.Text = "メニューに戻る";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(410, 372);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(107, 33);
-            this.button5.TabIndex = 8;
-            this.button5.Text = "メニューに戻る";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // CustomerList
+            // GoodsList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(539, 417);
-            this.Controls.Add(this.button5);
+            this.ClientSize = new System.Drawing.Size(472, 427);
             this.Controls.Add(this.button4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
-            this.Name = "CustomerList";
-            this.Text = "CustomerList";
+            this.Name = "GoodsList";
+            this.Text = "GoodsList";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -184,17 +168,15 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
     }
 }
