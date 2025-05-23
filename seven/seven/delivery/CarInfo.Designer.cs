e972376f6623cd08e321ef7409c207710d430851
honda-yuki-9134 <585-student019@la-study.com>
2025-05-23 13:06:36 +0900

    form

--- seven/seven/delivery/CarInfo.Designer.cs
+++ seven/seven/delivery/CarInfo.Designer.cs
@@ -30,15 +30,15 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
@@ -59,6 +59,28 @@
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "車両ナンバー";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "積載量";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.HeaderText = "稼働状況";
+            this.Column3.Name = "Column3";
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -71,7 +93,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(41, 384);
+            this.button1.Location = new System.Drawing.Point(19, 383);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(181, 43);
             this.button1.TabIndex = 3;
@@ -81,7 +103,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(228, 384);
+            this.button2.Location = new System.Drawing.Point(215, 384);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(181, 43);
             this.button2.TabIndex = 4;
@@ -112,28 +134,6 @@
             // errorProvider1
             // 
             this.errorProvider1.ContainerControl = this;
-            // 
-            // Column1
-            // 
-            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column1.HeaderText = "車両ナンバー";
-            this.Column1.Name = "Column1";
-            this.Column1.ReadOnly = true;
-            // 
-            // Column2
-            // 
-            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column2.HeaderText = "積載量";
-            this.Column2.Name = "Column2";
-            this.Column2.ReadOnly = true;
-            // 
-            // Column3
-            // 
-            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column3.HeaderText = "稼働状況";
-            this.Column3.Name = "Column3";
-            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
             // 
             // CarInfo
             // 
--- seven/seven/delivery/OrderEdit.cs
+++ seven/seven/delivery/OrderEdit.cs
@@ -1,20 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace seven
 {
     public partial class OrderEdit : Form
     {
+        private readonly string sqlConnectionString =
+             ConfigurationManager.ConnectionStrings["delivery_system"].ConnectionString;
         public OrderEdit()
         {
             InitializeComponent();
+            CustomerRead();
+        }
+        public void CustomerRead()
+        {
+            int n=0;
+            StringBuilder sql = new StringBuilder();
+            sql.Append("SELECT customer_id,customer_name FROM customer ");
+            SqlConnection con = new SqlConnection();
+                con.ConnectionString = sqlConnectionString;
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = sql.ToString();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    comboBox1.Items.Add($"{reader["customer_id"]} { reader["customer_name"]}");
+                    //comboBox1.DisplayMember =reader["customer_name"].ToString();
+                    //comboBox1.ValueMember=reader["customer_id"].ToString();
+                }
+        }
+        public void Insert()
+        {
+            string sql = "INSERT INTO order_item(customer_id,sales_id,order_date,emp_id,okihai,okibasho,delivered,cancel)" +
+               "VALUES('" + comboBox1 + "'," + comboBox2+ ","+ comboBox1+" )";
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = sqlConnectionString;
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = sql;
+            cmd.ExecuteNonQuery();
+            con.Close();
+            this.Close();
         }
     }
+    
 }
